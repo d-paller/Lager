37e740d145df3010702a3324bf58559695af2e2d
d-paller <darthdollar@gmail.com>
2016-12-17 15:48:55 -0500

    Add project files.

---
+++ src/Lager/Models/AdminUser.cs
@@ -0,0 +1,38 @@
+﻿using Lager.CoreFiles.Interfaces;
+
+namespace LagerCore.Core.Models
+{
+    public class AdminUser : IUser
+    {
+        /// <summary>
+        /// The hashed and ecrypted password of the user
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// The hashed username of the user
+        /// </summary>
+        public string Username { get; set; }
+
+        /// <summary>
+        /// The name of the user
+        /// </summary>
+        public string Name { get; set; }
+
+        private bool Admin = true;
+
+        /// <summary>
+        /// If the user is active or not.  The default value is true.
+        /// </summary>
+        public bool IsActive { get; set; } = true;
+
+        /// <summary>
+        /// Checks to see if the user is an admin or not.
+        /// </summary>
+        /// <returns>True if the user is an admin, false if not.</returns>
+        public bool IsAdmin()
+        {
+            return Admin;
+        }
+    }
+}
---
+++ src/Lager/Models/Part.cs
@@ -0,0 +1,41 @@
+﻿using Lager.CoreFiles.Interfaces;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LagerCore.Core.Models
+{
+    public class Part : IPart
+    {
+        /// <summary>
+        /// Name of the part
+        /// </summary>
+        [Required]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The ID of the part
+        /// </summary>
+        public int PartId { get; set; }
+
+        /// <summary>
+        /// If the part is active or not.  Default is true
+        /// </summary>
+
+        public bool IsActive { get; set; } = true;
+
+        /// <summary>
+        /// The date that the part was added
+        /// </summary>
+        public DateTime DateAdded { get; set; }
+
+        /// <summary>
+        /// A short description of the part
+        /// </summary>
+        [Required]
+        [StringLength(100)]
+        public string Description { get; set; }
+
+        public string PurchaseUrl { get; set; }
+
+    }
+}
