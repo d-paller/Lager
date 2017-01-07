21ac314271d9910b220355778e163ec374ecc1f4
Leo <leoisawe@gmail.com>
2017-01-07 18:46:08 -0500

    repo

---
+++ src/LagerTests/RepoTest.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lager.Services;
+using Lager.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace LagerTests
+{
+    [TestClass]
+    public class RepoTest
+    {
+    }
+}
