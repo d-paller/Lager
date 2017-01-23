ccdec43095d75e612ad6ff7f092b807a4d641368
Ian <mhfloro@ius.edu>
2017-01-23 16:36:27 -0500

    Create partial

--- src/Lager/Controllers/AdminController.cs
+++ src/Lager/Controllers/AdminController.cs
@@ -5,20 +5,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Lager.Models;
+using Lager.Services.Repositories;
+using Lager.Interfaces;
 
 namespace Lager.Controllers
 {
     public class AdminController : Controller
     {
+
+        // DataAccess partList = new DataAccess();
+        //IList<DataAccess> partInventory partList.GetParts();
+        //ViewData.Model = new ViewResult
+        //{ partInventory = partList; };
+        //return View(Part);
+
+        //private readonly IPartRepository partRepo;
+
         private User user = new User() { IsActive = true, Name = "Test", Username = "TestUserName", Admin = true };
 
         public IActionResult Index()
         {
-            // DataAccess partList = new DataAccess();
-            //IList<DataAccess> partInventory partList.GetParts();
-            //ViewData.Model = new ViewResult
-            //{ partInventory = partList; };
-            //return View(Part);
+            
+
             return View();
         }
 
@@ -29,6 +37,8 @@ namespace Lager.Controllers
 
         public IActionResult Create()
         {
+            //how to post?
+            //_PartRepository.AddPart(item);
             return View();
         }
 
