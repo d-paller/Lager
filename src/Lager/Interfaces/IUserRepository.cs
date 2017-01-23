1eefecaabd898f4ad2dc13238eac76aaa949021b
d-paller <darthdollar@gmail.com>
2017-01-23 17:28:29 -0500

    Add IUserRepo

---
+++ src/Lager/Interfaces/IUserRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lager.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<IUser> GetUserByUsername(string userName);
+
+        Task<IUser> GetUserById(int Id);
+
+        Task AddUser(IUser user);
+
+        Task<bool> UserExists(string userName);
+
+        Task<bool> UserExists(int userId);
+    }
+}
