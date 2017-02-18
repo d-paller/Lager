cf0b52771b676e1b0247fb012123176ed5e7daba
d-paller <darthdollar@gmail.com>
2017-02-18 12:08:53 -0500

    Add Roster UI.  Add File uplaod

---
+++ src/Lager/Controllers/RosterController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lager.Controllers
+{
+    public class RosterController : Controller
+    {
+        private IHostingEnvironment _environment;
+
+        public RosterController(IHostingEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        public IActionResult UploadFile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadFile(IFormFile file)
+        {
+            var uploads = Path.Combine(_environment.WebRootPath, "RosterUploads");
+
+            if(file.Length > 0)
+            {
+                using(var fileStream = new FileStream(Path.Combine(uploads, file.FileName),
+                    FileMode.Create))
+                {
+                    await file.CopyToAsync(fileStream);
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ src/Lager/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lager.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+
+        public int Id { get; set; }
+
+        public string SisLoginId { get; set; }
+
+        public string Section { get; set; }
+
+    }
+}
