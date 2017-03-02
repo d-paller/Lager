4c328752746fd2590f3741c7c8880afa63263ef5
Ian <mhfloro@ius.edu>
2017-03-02 11:52:38 -0500

    More UI tweaks

--- src/Lager/Controllers/AdminController.cs
+++ src/Lager/Controllers/AdminController.cs
@@ -188,7 +188,7 @@ namespace Lager.Controllers
         public IActionResult edit()
         {
             PartViewModel model = new PartViewModel();
-            return View("Edit");
+            return View(model);
         }
 
         public IActionResult Test()
