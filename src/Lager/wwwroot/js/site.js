9d938d63be61903b73b5abbba25fc73f3012e267
Leo <leoisawe@gmail.com>
2017-03-22 18:15:35 -0400

    jquerry

--- src/Lager/wwwroot/js/site.js
+++ src/Lager/wwwroot/js/site.js
@@ -2,10 +2,35 @@
     this.slideUp(1000).fadeOut(500);
 });
 
-$("btn btn-info").click(function(){
-    var x =document.forms
-})
+$(".edit-Btn").click(function () {
+    $.ajax({
+        url: "/Admin/Edit",
+        type: "GET",
+        data: { id: $(this).data("part-id") },
+        success: function () {
+            alert("success");
+            $(location).attr('href', "/Admin/Edit", $(this).data("part-id"))
+        },
+        error: function () {
+            alert("error" + window.u);
+        }
+    })
+});
 
+
+$(".btn-danger").click(function () {
+    $.ajax({
+        url: "/Admin/RemoveItem",
+        type: "POST",
+        data: { id: $(this).data("part-id") },
+        success: function () {
+            alert("success");
+        },
+        error: function () {
+            alert("error" + window.u);
+        }
+    })
+})
 
 $(".expand-btn").click(function () {
     var id = $(this).data("item-id");
